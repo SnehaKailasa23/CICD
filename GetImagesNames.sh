f50f240533aa95eaadfb07b4fdea56fe5fd284c8
SnehaKailasa23 <35054580+SnehaKailasa23@users.noreply.github.com>
2018-01-08 11:10:44 +0530

    Create GetImagesNames.sh

---
+++ GetImagesNames.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+j=0
+for i in `grep -n "image" ./FortnaCICD/.env`; do
+if [[ $i != *"robot"* ]]; then
+Images[$j]=`cut -d '=' -f2 <<<$i`
+j=`expr $j + 1`
+fi
+done
+for z in "${Images[@]}"; do
+echo $z
+done
