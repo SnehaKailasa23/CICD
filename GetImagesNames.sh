870deb6748135cb4f2219d83327c75601b196fd0
SnehaKailasa23 <35054580+SnehaKailasa23@users.noreply.github.com>
2018-01-08 12:52:53 +0530

    Update GetImagesNames.sh

--- GetImagesNames.sh
+++ GetImagesNames.sh
@@ -1,18 +1,18 @@
-##!/bin/bash
-#j=0
-#for i in `grep -n "image" .env`; do
-#if [[ $i != *"robot"* ]]; then
-#Images[$j]=`cut -d '=' -f2 <<<$i`
-#j=`expr $j + 1`
-#fi
-#done
-#for z in "${Images[@]}"; do
-#echo $z
-#done
-
 #!/bin/bash
-for i in `grep -n "image" ./FortnaCICD/.env`; do
+j=0
+for i in `grep -n "image" .env`; do
 if [[ $i != *"robot"* ]]; then
-echo `cut -d '=' -f2 <<<$i`
+Images[$j]=`cut -d '=' -f2 <<<$i`
+j=`expr $j + 1`
 fi
 done
+for z in "${Images[@]}"; do
+echo $z
+done
+
+##!/bin/bash
+#for i in `grep -n "image" ./FortnaCICD/.env`; do
+#if [[ $i != *"robot"* ]]; then
+#echo `cut -d '=' -f2 <<<$i`
+#fi
+#done
